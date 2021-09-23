d9c670b1f68794b98d0f0539d726b8f2b3097a3f
LeHamzaSh <hrsheikh05@gmail.com>
2021-09-23 15:14:36 +0400

    Fixed Rendering Bugs

---
+++ src/components/ViewAllTask.vue
@@ -0,0 +1,17 @@
+<template>
+    <h1>{{ msg }}</h1>
+    <h2>Hello everyone</h2>
+</template>
+
+<script>
+    export default{
+        name: "ViewAllTask",
+        props: {
+            msg: String
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
--- src/views/About.vue
+++ src/views/About.vue
@@ -1,5 +1,16 @@
 <template>
   <div class="about">
-    <h1>This is an about page</h1>
+    <AddTask/>
   </div>
 </template>
+
+<script>
+  import AddTask from '@/components/AddTask.vue'
+
+export default {
+  name: 'About',
+  components: {
+    AddTask
+  }
+}
+</script>
